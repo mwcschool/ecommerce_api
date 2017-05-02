d1d993c063ad5e33ac940fbbcd309cec5a558084
Matteo Canu <matteo@soraph.me>
2017-05-02 16:26:09 +0200

    Added api resource endpoints.

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 from models import database
+
 from views import item
+from views.order import OrderResource, OrdersResource
 from views.user import UserResource, UsersResource
+
+import views.order
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,3 +29,5 @@ api.add_resource(item.ItemsResource, '/items/')
 api.add_resource(item.ItemResource, '/item/<uuid:item_id>')
 api.add_resource(UsersResource, '/users/')
 api.add_resource(UserResource, '/users/<uuid:user_id>')
+api.add_resource(OrdersResource, '/orders/')
+api.add_resource(OrderResource, '/orders/<uuid:order_id>')
