998c84215ff24b30655be94a9138a1ed76133ae1
Matteo Canu <matteo@soraph.me>
2017-05-02 15:30:25 +0200

    Added get method to OrderResource.

--- views/order.py
+++ views/order.py
@@ -8,3 +8,11 @@ from models import Order
 class OrdersResource(Resource):
     def get(self):
         return [order.json() for order in Order.select()], OK
+
+
+class OrderResource(Resource):
+    def get(self, order_id):
+        try:
+            return Order.get(order_id=order_id).json(), OK
+        except Order.DoesNotExist:
+            return None, NOT_FOUND
