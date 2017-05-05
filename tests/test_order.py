32b5235e68dab38bb47475c5773eb9099b7ea640
Vincenzo Volpe <vincenzovolpe10@icloud.com>
2017-05-05 14:37:53 +0200

    fixato il test_get_order e aggiunto test_create_order_success

--- tests/test_order.py
+++ tests/test_order.py
@@ -47,3 +47,20 @@ class TestOrders:
 
 		resp = self.app.get('/orders/')
 		assert resp.status_code == OK
+		assert json.loads(resp.data.decode()) == [ord1.json(), ord2.json()]
+
+	def test_create_order__success(self):
+		source_order = {
+			'total_price':type=is_decimal,
+			'user':type=int,
+		}
+		resp = self.app.post('/orders/', data=source_order)
+		assert resp.status_code == CREATED
+		order = json.loads(resp.data.decode())
+
+		assert len(Order.select()) == 1
+		assert Order.get(order_id=order['order_id']).json() == order
+		order.pop('order_id')
+		assert order == source_order
+
+
