ff115c60390d2c4345cd92ff103f0be4344704b8
Vincenzo Volpe <vincenzovolpe10@icloud.com>
2017-05-08 15:07:29 +0200

    Added test test_create_order__success().

--- tests/test_order.py
+++ tests/test_order.py
@@ -48,3 +48,27 @@ class TestOrders:
         resp = self.app.get('/orders/')
         assert resp.status_code == OK
         assert json.loads(resp.data.decode()) == [ord1.json(), ord2.json()]
+
+
+    def test_create_order__success(self):
+        usr1 = User.create(
+                user_id=str(uuid.uuid4()),
+                first_name='Name',
+                last_name='Surname',
+                email='email@domain.com',
+                password='password'
+            )
+
+        source_order = {
+            'total_price': 10,
+            'user': usr1.id
+        }
+
+        resp = self.app.post('/orders/', data=source_order)
+        assert resp.status_code == CREATED
+        order = json.loads(resp.data.decode())
+
+        assert len(Order.select()) == 1
+        assert Order.get(order_id=order['order_id']).json() == order
+        order.pop('order_id')
+        assert order == source_order
