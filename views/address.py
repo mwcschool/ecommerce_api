6c1ada4a6d0563e4cfc85d7f5c1a62f11bba7360
Alberto Paolucci <alberto.paolucci@icloud.com>
2017-05-12 12:00:28 +0200

    Added PUT method

--- views/address.py
+++ views/address.py
@@ -80,13 +80,22 @@ class AddressResource(Resource):
         parser.add_argument('phone', type=non_empty_str, required=True)
         args = parser.parse_args(strict=True)
 
+        if len(args['nation']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['city']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['postal_code']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['local_address']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['phone']) < 3:
+            return '', BAD_REQUEST
 
-        obj.nation=args['nation']
-        obj.city=args['city']
-        obj.postal_code=args['postal_code']
-        obj.local_address=args['local_address']
-        obj.phone=args['phone']
-
+        obj.nation = args['nation']
+        obj.city = args['city']
+        obj.postal_code = args['postal_code']
+        obj.local_address = args['local_address']
+        obj.phone = args['phone']
         obj.save()
 
         return obj.json(), CREATED
