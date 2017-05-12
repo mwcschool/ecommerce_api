1cbb41872096ece490afd64cb8ad9f0d9b61c7a5
F.Vecchi <fv284@hotmail.com>
2017-05-12 11:54:33 +0200

    Created functions and methods for addresses processing

--- views/address.py
+++ views/address.py
@@ -12,8 +12,9 @@ def non_empty_str(val, name):
 
 
 class AddressesResource(Resource):
-    def post(self, user_id):
+    def post(self):
         parser = reqparse.RequestParser()
+        parser.add_argument('user_id', type=non_empty_str, required=True)
         parser.add_argument('nation', type=non_empty_str, required=True)
         parser.add_argument('city', type=non_empty_str, required=True)
         parser.add_argument('postal_code', type=non_empty_str, required=True)
@@ -28,6 +29,18 @@ class AddressesResource(Resource):
                 address_id=uuid.uuid4(), user=user_id, nation=args['nation'],
                 city=args['city'], postal_code=args['postal_code'],
                 local_address=args['local_address'], phone=args['phone'])
+
+        query = User.select().where(User.user_id == args['user_id'])
+
+        if query.exists():
+            obj = Address.create(
+                user=query[0],
+                address_id=uuid.uuid4(),
+                nation=args['nation'],
+                city=args['city'],
+                postal_code=args['postal_code'],
+                local_address=args['local_address'],
+                phone=args['phone'])
 
             return obj.json(), CREATED
         else:
@@ -48,6 +61,25 @@ class AddressResource(Resource):
             obj = Address.get(address_id=address_id)
         except Address.DoesNotExist:
             return None, NOT_FOUND
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('nation', type=non_empty_str, required=True)
+        parser.add_argument('city', type=non_empty_str, required=True)
+        parser.add_argument('postal_code', type=non_empty_str, required=True)
+        parser.add_argument('local_address', type=non_empty_str, required=True)
+        parser.add_argument('phone', type=non_empty_str, required=True)
+        args = parser.parse_args(strict=True)
+
+
+        obj.nation=args['nation']
+        obj.city=args['city']
+        obj.postal_code=args['postal_code']
+        obj.local_address=args['local_address']
+        obj.phone=args['phone']
+
+        obj.save()
+
+        return obj.json(), CREATED
 
     def delete(self, address_id):
         try:
