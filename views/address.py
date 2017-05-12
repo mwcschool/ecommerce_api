a66ba677503f38a06c3420b47d7b0a68c297dc9b
Alberto Paolucci <alberto.paolucci@icloud.com>
2017-05-12 12:00:28 +0200

    Added PUT method

--- views/address.py
+++ views/address.py
@@ -59,6 +59,34 @@ class AddressResource(Resource):
         except Address.DoesNotExist:
             return None, NOT_FOUND
 
+        parser = reqparse.RequestParser()
+        parser.add_argument('nation', type=non_empty_str, required=True)
+        parser.add_argument('city', type=non_empty_str, required=True)
+        parser.add_argument('postal_code', type=non_empty_str, required=True)
+        parser.add_argument('local_address', type=non_empty_str, required=True)
+        parser.add_argument('phone', type=non_empty_str, required=True)
+        args = parser.parse_args(strict=True)
+
+        if len(args['nation']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['city']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['postal_code']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['local_address']) < 3:
+            return '', BAD_REQUEST
+        elif len(args['phone']) < 3:
+            return '', BAD_REQUEST
+
+        obj.nation = args['nation']
+        obj.city = args['city']
+        obj.postal_code = args['postal_code']
+        obj.local_address = args['local_address']
+        obj.phone = args['phone']
+        obj.save()
+
+        return obj.json(), CREATED
+
     def delete(self, address_id):
         try:
             obj = Address.get(address_id=address_id)
