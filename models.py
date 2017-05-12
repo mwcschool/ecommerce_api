66c36562e2b90aeebf6708b465cc5d5565ede93c
Korda Iryna <irynakorda@gmail.com>
2017-05-12 10:00:07 +0200

    Fixed function verify password

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from peewee import Model, SqliteDatabase
 from peewee import DecimalField, TextField, CharField
 from peewee import UUIDField, ForeignKeyField, IntegerField
+from passlib.hash import pbkdf2_sha256
 
 database = SqliteDatabase('database.db')
 
@@ -29,8 +30,8 @@ class User(BaseModel):
             'user_id': str(self.user_id)
         }
 
-    def verify_email(self):
-        pbkdf2_sha256.verify(origin_password, self.password)
+    def verify_password(self, origin_password):
+        return pbkdf2_sha256.verify(origin_password, self.password)
 
 
 class Address(BaseModel):
