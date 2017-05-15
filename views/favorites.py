5decfbad1bb4a91d18af90208168f67776d1878b
Francesco Cambi <francesco100@hotmail.it>
2017-05-15 17:57:27 +0200

    Added try/except for get Favorites while db is empty

--- views/favorites.py
+++ views/favorites.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, reqparse
 # from http.client import CREATED
 # from http.client import NO_CONTENT
-# from http.client import NOT_FOUND
+from http.client import NOT_FOUND
 from http.client import OK
 # from http.client import BAD_REQUEST
 from models import Item, User, Favorites
@@ -12,7 +12,10 @@ import utils
 class FavoritesResource(Resource):
     def get(self):
         # TODO: we will have a user from auth here.
-        user = User.select().get()
+        try:
+            user = User.select().get()
+        except User.DoesNotExist:
+            return None, NOT_FOUND
         return user.get_favorite_items(), OK
 
 
