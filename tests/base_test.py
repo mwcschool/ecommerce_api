f67bab84d69ee9476abc4306010cc80885207518
Matteo Canu <matteo@soraph.me>
2017-05-18 15:40:11 +0200

    Added helper method create_user().

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,6 +1,7 @@
 from models import Item, User, Address, Order, OrderItem
 from peewee import SqliteDatabase
 from app import app
+import uuid
 
 
 class BaseTest:
@@ -19,3 +20,13 @@ class BaseTest:
     def setup_method(self):
         for table in self.tables:
             table.delete().execute()
+
+    def create_user(first_name="First Name", last_name="Last name",
+                    email="email@domain.com", password="p4ssw0rd"):
+        return User.create(
+            user_id=str(uuid.uuid4()),
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password,
+        )
