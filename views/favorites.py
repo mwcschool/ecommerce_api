5f0e83847fe33059531bd61019b63148c3e2bfa0
Francesco Cambi <francesco100@hotmail.it>
2017-05-18 18:00:44 +0200

    Added delete in Favorite Resource #NeedToFinish

--- views/favorites.py
+++ views/favorites.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-# from http.client import CREATED
+from http.client import CREATED
 # from http.client import NO_CONTENT
 from http.client import NOT_FOUND
 from http.client import OK
@@ -8,8 +8,9 @@ from models import Item, User, Favorites
 import uuid
 import utils
 
+
 def check_uuid_is_in_(id_check):
-    #if id_check not
+    # if id_check not
     pass
 
 
@@ -30,14 +31,35 @@ class FavoritesResource(Resource):
         if not User.exists_uuid(args['id_user']) and not Item.exists_uuid(args['id_item']):
             return None, NOT_FOUND
 
-
         obj = Favorites.create(
-            item=args['id_item'],
-            user=args['id_user']
+            item=Item.get(Item.item_id == args['id_item']),
+            user=User.get(User.user_id == args['id_user']),
         )
         return obj.json(), CREATED
 
 
 class FavoriteResource(Resource):
-    def get(self):
+    def delete(self):
+        # TODO: we will have a user from auth here.
+        try:
+            user = User.select().get()
+        except User.DoesNotExist:
+            return None, NOT_FOUND
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('id_item', type=type(uuid.uuid4()), required=True)
+        args = parser.parse_args(strict=True)
+        if not Item.exists_uuid(args['id_item']):
+            return None, NOT_FOUND
+
+        query = Favorites.select()\
+            .join(Item)\
+            .where(Favorites.item == Item.item_id)\
+            .join(User)\
+            .where(Favorites.user == User.user_id)\
+            # .on(Favorites.item ==  )
+
+        # if not Favorites.s
+        #    return None, NOT_FOUND
+
         True
