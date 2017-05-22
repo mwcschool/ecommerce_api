a976d667b1fa6c0d1387253ebb03feae54bb3aaf
Alberto Paolucci <alberto.paolucci@icloud.com>
2017-05-22 16:35:34 +0200

    Code optimization

--- views/address.py
+++ views/address.py
@@ -2,23 +2,18 @@ import uuid
 from models import User, Address
 from http.client import CREATED, NOT_FOUND, NO_CONTENT, BAD_REQUEST, OK
 from flask_restful import Resource, reqparse
-
-
-def non_empty_str(val, name):
-    if not str(val).strip():
-        raise ValueError('The argument {} is not empty'.format(name))
-    return str(val)
+import utils
 
 
 class AddressesResource(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('user_id', type=non_empty_str, required=True)
-        parser.add_argument('nation', type=non_empty_str, required=True)
-        parser.add_argument('city', type=non_empty_str, required=True)
-        parser.add_argument('postal_code', type=non_empty_str, required=True)
-        parser.add_argument('local_address', type=non_empty_str, required=True)
-        parser.add_argument('phone', type=non_empty_str, required=True)
+        parser.add_argument('user_id', type=utils.non_empty_str, required=True)
+        parser.add_argument('nation', type=utils.non_empty_str, required=True)
+        parser.add_argument('city', type=utils.non_empty_str, required=True)
+        parser.add_argument('postal_code', type=utils.non_empty_str, required=True)
+        parser.add_argument('local_address', type=utils.non_empty_str, required=True)
+        parser.add_argument('phone', type=utils.non_empty_str, required=True)
         args = parser.parse_args(strict=True)
 
         if len(args['nation']) < 3:
@@ -33,40 +28,45 @@ class AddressesResource(Resource):
             return '', BAD_REQUEST
 
         try:
-            check_user = User.get(User.user_id == args['user_id'])
+            user = User.get(User.user_id == args['user_id'])
         except User.DoesNotExist:
             return '', BAD_REQUEST
 
-        obj = Address.create(
-            address_id=uuid.uuid4(), user=check_user, nation=args['nation'],
-            city=args['city'], postal_code=args['postal_code'],
-            local_address=args['local_address'], phone=args['phone'])
+        address = Address.create(
+            address_id=uuid.uuid4(),
+            user=user,
+            nation=args['nation'],
+            city=args['city'],
+            postal_code=args['postal_code'],
+            local_address=args['local_address'],
+            phone=args['phone'],
+        )
 
-        return obj.json(), CREATED
+        return address.json(), CREATED
 
 
 class AddressResource(Resource):
     def get(self, address_id):
         try:
-            obj = Address.get(Address.address_id == address_id)
+            address = Address.get(Address.address_id == address_id)
         except Address.DoesNotExist:
             return None, NOT_FOUND
 
-        return obj.json(), OK
+        return address.json(), OK
 
     def put(self, address_id):
         try:
-            obj = Address.get(Address.address_id == address_id)
+            address = Address.get(Address.address_id == address_id)
         except Address.DoesNotExist:
             return None, NOT_FOUND
 
         parser = reqparse.RequestParser()
-        parser.add_argument('user_id', type=non_empty_str, required=True)
-        parser.add_argument('nation', type=non_empty_str, required=True)
-        parser.add_argument('city', type=non_empty_str, required=True)
-        parser.add_argument('postal_code', type=non_empty_str, required=True)
-        parser.add_argument('local_address', type=non_empty_str, required=True)
-        parser.add_argument('phone', type=non_empty_str, required=True)
+        parser.add_argument('user_id', type=utils.non_empty_str, required=True)
+        parser.add_argument('nation', type=utils.non_empty_str, required=True)
+        parser.add_argument('city', type=utils.non_empty_str, required=True)
+        parser.add_argument('postal_code', type=utils.non_empty_str, required=True)
+        parser.add_argument('local_address', type=utils.non_empty_str, required=True)
+        parser.add_argument('phone', type=utils.non_empty_str, required=True)
         args = parser.parse_args(strict=True)
 
         if len(args['nation']) < 3:
@@ -80,23 +80,23 @@ class AddressResource(Resource):
         elif len(args['phone']) < 3:
             return '', BAD_REQUEST
 
-        if str(obj.user.user_id) == str(args['user_id']):
-            obj.nation = args['nation']
-            obj.city = args['city']
-            obj.postal_code = args['postal_code']
-            obj.local_address = args['local_address']
-            obj.phone = args['phone']
-            obj.save()
+        if str(address.user.user_id) == args['user_id']:
+            address.nation = args['nation']
+            address.city = args['city']
+            address.postal_code = args['postal_code']
+            address.local_address = args['local_address']
+            address.phone = args['phone']
+            address.save()
 
-            return obj.json(), CREATED
+            return address.json(), CREATED
         else:
             return '', BAD_REQUEST
 
     def delete(self, address_id):
         try:
-            obj = Address.get(Address.address_id == address_id)
+            address = Address.get(Address.address_id == address_id)
         except Address.DoesNotExist:
             return None, NOT_FOUND
 
-        obj.delete_instance()
+        address.delete_instance()
         return None, NO_CONTENT
