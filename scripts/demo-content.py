1dcd5b6e5e9f679e47776bda18cd79460c33970e
Alberto Paolucci <alberto.paolucci@icloud.com>
2017-05-26 10:10:15 +0200

    Changed id in uuid

--- scripts/demo-content.py
+++ scripts/demo-content.py
@@ -15,7 +15,7 @@ def main():
 
     for _ in range(10):
         item = Item.create(
-            item_id=fake.uuid4(),
+            uuid=fake.uuid4(),
             name=fake.word(),
             price=fake.random_int(),
             description=fake.sentence(),
@@ -25,7 +25,7 @@ def main():
 
     for _ in range(20):
         user = User.create(
-            user_id=fake.uuid4(),
+            uuid=fake.uuid4(),
             first_name=fake.first_name(),
             last_name=fake.last_name(),
             email=fake.email(),
@@ -36,7 +36,7 @@ def main():
     for user in users_list:
         for x in range(randint(1, 3)):
             Address.create(
-                address_id=fake.uuid4(),
+                uuid=fake.uuid4(),
                 user=user,
                 nation=fake.country(),
                 city=fake.city(),
@@ -57,7 +57,7 @@ def main():
             order_item_subtotal = 0
 
             order = Order.create(
-                order_id=fake.uuid4(),
+                uuid=fake.uuid4(),
                 total_price=order_total_price,
                 user=user,
                 )
