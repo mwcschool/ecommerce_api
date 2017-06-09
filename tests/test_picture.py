c6d7779d55511b5f68bc4b4d625f5e3304335cf1
Luca Bandini <luca.bandini@outlook.it>
2017-06-09 17:46:35 +0200

    added test_get_picture

---
+++ tests/test_picture.py
@@ -0,0 +1,20 @@
+import json
+from peewee import SqliteDatabase
+from http.client import CREATED
+from http.client import NO_CONTENT
+from http.client import NOT_FOUND
+from http.client import OK
+from http.client import BAD_REQUEST
+from models import Picture
+from models import Item
+from app import app
+import uuid
+from .base_test import BaseTest
+
+class TestPicture(BaseTest):
+
+    def test_get_picture(self):
+        picture1 = self.create_item_picture()
+
+        resp = self.app.get('/pictures/{}'.format(picture1.uuid))
+        assert resp.status_code == OK
