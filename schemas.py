5232e1f81339ee336576fba8e4a2f37030bedadd
Alberto Paolucci <alberto.paolucci@icloud.com>
2017-06-09 15:38:12 +0200

    Fixed and improved schema validation for string and numeric fields

--- schemas.py
+++ schemas.py
@@ -1,29 +1,40 @@
-from utils import non_empty_str
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError
+
+
+def check_empty_str(value):
+    if str(value).strip() == '':
+        raise ValidationError('String field must not be empty')
+    return True
+
+
+def check_negative_number(value):
+    if value < 0:
+        raise ValidationError('Number must be greater than or equal to zero')
+    return True
 
 
 class ItemSchema(Schema):
     uuid = fields.UUID(required=True, dump_only=True)
-    name = fields.Str(required=True, validate=non_empty_str)
-    price = fields.Decimal(required=True)
-    description = fields.Str(required=True, validate=non_empty_str)
-    category = fields.Str(required=True, validate=non_empty_str)
-    availability = fields.Int(required=True, default=0)
+    name = fields.Str(required=True, validate=check_empty_str)
+    price = fields.Decimal(required=True, validate=check_negative_number)
+    description = fields.Str(required=True, validate=check_empty_str)
+    category = fields.Str(required=True, validate=check_empty_str)
+    availability = fields.Int(required=True, validate=check_negative_number)
 
 
 class UserSchema(Schema):
     uuid = fields.UUID(required=True, dump_only=True)
-    first_name = fields.Str(required=True)
-    last_name = fields.Str(required=True)
-    email = fields.Str(required=True, validate=non_empty_str)
-    password = fields.Str(required=True, validate=non_empty_str, load_only=True)
+    first_name = fields.Str(required=True, validate=check_empty_str)
+    last_name = fields.Str(required=True, validate=check_empty_str)
+    email = fields.Str(required=True, validate=check_empty_str)
+    password = fields.Str(required=True, validate=check_empty_str, load_only=True)
 
 
 class AddressSchema(Schema):
     uuid = fields.UUID(required=True, dump_only=True)
     user = fields.UUID(required=True, attribute='user.uuid')
-    nation = fields.Str(required=True)
-    city = fields.Str(required=True)
-    postal_code = fields.Str(required=True)
-    local_address = fields.Str(required=True)
-    phone = fields.Str(required=True)
+    nation = fields.Str(required=True, validate=check_empty_str)
+    city = fields.Str(required=True, validate=check_empty_str)
+    postal_code = fields.Str(required=True, validate=check_empty_str)
+    local_address = fields.Str(required=True, validate=check_empty_str)
+    phone = fields.Str(required=True, validate=check_empty_str)
