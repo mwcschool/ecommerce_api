5db152dec49cd91e9a7d2e0d799a6349ad0f2b98
F.Vecchi <fv284@hotmail.com>
2017-06-12 17:13:04 +0200

    Built jsonschemas validation tests file

---
+++ tests/test_jsonschema.py
@@ -0,0 +1,118 @@
+import json
+import uuid
+from models import BaseModel, Item, User, Address
+from .base_test import BaseTest
+from jsonschema import ValidationError
+import pytest
+
+class TestValidateJsonschema:
+    def test_validate_item_json__success(self):
+        data = {
+            'name': 'Item one',
+            'price': 15,
+            'description': 'desc1',
+            'category': 'poligoni',
+            'availability': 11,
+        }
+
+        Item.verify_json(data)
+
+    def test_validate_item_json__failure_wrong_type_field(self):
+        data = {
+            'name': 'Item one',
+            'price': '15',
+            'description': 'desc1',
+            'category': 'poligoni',
+            'availability': 11,
+        }
+
+
+        with pytest.raises(ValidationError):
+            Item.verify_json(data)
+
+    def test_validate_item_json__failure_missing_field(self):
+        data = {
+            'name': 'Item one',
+            'price': 15,
+            'description': 'desc1',
+            'availability': 11,
+        }
+
+        with pytest.raises(ValidationError):
+            Item.verify_json(data)
+
+
+    def test_validate_user_json__success(self):
+        data = {
+            'first_name': 'Anna',
+            'last_name': 'Markis',
+            'email': 'anna@markis.com',
+            'password': '1234567',
+            }
+
+        User.verify_json(data)
+
+
+    def test_validate_user_json__failure_wrong_type_field(self):
+        data = {
+            'first_name': 'Anna',
+            'last_name': 'Markis',
+            'email': 'anna@markis.com',
+            'password': 1234567,
+            }
+
+        with pytest.raises(ValidationError):
+            User.verify_json(data)
+
+
+    def test_validate_user_json__failure_missing_field(self):
+        data = {
+            'first_name': 'Anna',
+            'email': 'anna@markis.com',
+            'password': '1234567',
+            }
+
+
+
+        with pytest.raises(ValidationError):
+            User.verify_json(data)
+
+
+    def test_validate_address_json__success(self):
+        data = {
+            'user': str(uuid.uuid4()),
+            'nation': 'Italia',
+            'city': 'Prato',
+            'postal_code': '59100',
+            'local_address': 'Via Roncioni 10',
+            'phone': '0574100100',
+        }
+
+        Address.verify_json(data)
+
+
+    def test_validate_address_json__failure_wrong_type_field(self):
+        data = {
+            'user': str(uuid.uuid4()),
+            'nation': 'Italia',
+            'city': 'Prato',
+            'postal_code': 59100,
+            'local_address': 'Via Roncioni 10',
+            'phone': '0574100100',
+        }
+
+        with pytest.raises(ValidationError):
+            Address.verify_json(data)
+
+
+    def test_validate_address_json__failure_missing_field(self):
+        data = {
+            'user': str(uuid.uuid4()),
+            'nation': 'Italia',
+            'postal_code': '59100',
+            'local_address': 'Via Roncioni 10',
+            'phone': '0574100100',
+        }
+
+        with pytest.raises(ValidationError):
+            Address.verify_json(data)
