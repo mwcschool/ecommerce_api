520697d57b89edcd118c5aee201ae538d9229672
Luca Bandini <luca.bandini@outlook.it>
2017-06-12 18:08:11 +0200

    star texting with working  get and delete

--- tests/test_picture.py
+++ tests/test_picture.py
@@ -18,4 +18,32 @@ class TestPicture(BaseTest):
 
         resp = self.app.get('/pictures/{}'.format(picture1.uuid))
         assert resp.status_code == OK
+        #assert json.loads(resp.data.decode()) == [picture1.json()]
 
+    def test_get_picture_not_existing_picture(self):
+        resp = self.app.get('/pictures/{}'.format(uuid.uuid4()))
+        assert resp.status_code == NOT_FOUND
+
+    # def test_get_picture_wrong_extension(self):
+    #     picture1 = self.create_item_picture(extension='png')
+
+    #     resp = self.app.get('/pictures/{}'.format(picture1.uuid))
+    #     assert resp.status_code == BAD_REQUEST
+
+    def test_get_picture_wrong_item(self):
+        picture1 = self.create_item_picture(item)
+
+        resp = self.app.get('/pictures/{}'.format(uuid.uuid4()))
+        assert resp.status_code == NOT_FOUND
+
+    def test_delete_picture_succed(self):
+        picture1 = self.create_item_picture()
+
+        resp = self.app.delete('/pictures/{}'.format(picture1.uuid))
+        assert resp.status_code == NO_CONTENT
+        assert len(Picture.select()) == 0
+        resp = self.app.get('/pictures/{}'.format(picture1.uuid))
+        assert resp.status_code == NOT_FOUND
+
+    def test_delete_picture_failure_not_found(self):
+        pass
