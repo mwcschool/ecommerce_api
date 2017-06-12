5b7c230cfc17d9648191076f4328ee5a7ced44d9
Luca Bandini <luca.bandini@outlook.it>
2017-06-12 12:44:30 +0200

    new implement get with secure_filename

--- views/picture.py
+++ views/picture.py
@@ -2,20 +2,28 @@ from flask_restful import Resource
 from http.client import NOT_FOUND
 from http.client import OK
 from flask import send_from_directory, current_app
-from models import Picture
+from models import Picture, Item
 
 
 class PictureResource(Resource):
 
     def get(self, uuid):
         try:
-            picture = Picture.get(Picture.uuid == uuid)
+            picture =  Picture.get(Picture.uuid == uuid)
         except Picture.DoesNotExist:
             return None, NOT_FOUND
 
+        item = Item.get(picture.item)
+
         return send_from_directory(
-            current_app.config['UPLOADS_FOLDER'],
-            '{}.{}'.format(picture.uuid, picture.extension),
+            os.path.join(
+                current_app.config['UPLOADS_FOLDER'],
+                'items',
+                str(item.uuid),
+            ),
+
+            secure_filename('.'.join([str(picture.uuid), picture.extension]))
+            #'{}.{}'.format(str(picture.uuid), picture.extension),
         )
 
     def delete(self, uuid):
