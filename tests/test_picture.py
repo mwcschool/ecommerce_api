dda2f9bd2ab48734bedc53cb24174206b4eda743
Luca Bandini <luca.bandini@outlook.it>
2017-06-15 17:27:52 +0200

    add test_get_picture_wrong_item, test_get_picture_int_uuid, test_get_picture_wrong_extension

--- tests/test_picture.py
+++ tests/test_picture.py
@@ -5,11 +5,13 @@ from http.client import NO_CONTENT
 from http.client import NOT_FOUND
 from http.client import OK
 from http.client import BAD_REQUEST
+from .base_test import BaseTest
 from models import Picture
 from models import Item
 from app import app
 import uuid
-from .base_test import BaseTest
+import os
+
 
 class TestPicture(BaseTest):
 
@@ -24,17 +26,38 @@ class TestPicture(BaseTest):
         resp = self.app.get('/pictures/{}'.format(uuid.uuid4()))
         assert resp.status_code == NOT_FOUND
 
+    def test_get_picture_wrong_item(self):
+        item1 = self.create_item()
+        item2 = self.create_item()
+        picture1 = self.create_item_picture(item1)
+        #import pdb; pdb.set_trace()
+
+        resp = self.app.get('/pictures/{}'.format(item2.uuid))
+        assert resp.status_code == NOT_FOUND
+
+    def test_get_picture_int_uuid(self):
+        picture1 = self.create_item_picture()
+
+        resp = self.app.get('/pictures/{}'.format(int(picture1.uuid)))
+        assert resp.status_code == NOT_FOUND
+
+    def test_get_picture_wrong_extension(self):
+        test_image_path= os.path.join('.', 'tests', 'images', 'test_image.png')
+
+        picture1 = self.create_item_picture(extension='jpeg')
+
+        resp = self.app.get('/pictures/{}'.format(picture1.uuid))
+        assert resp.status_code == NOT_FOUND
+
+
+    #def test_image_path():
+        #pass
+
     # def test_get_picture_wrong_extension(self):
     #     picture1 = self.create_item_picture(extension='png')
 
     #     resp = self.app.get('/pictures/{}'.format(picture1.uuid))
     #     assert resp.status_code == BAD_REQUEST
-
-    def test_get_picture_wrong_item(self):
-        picture1 = self.create_item_picture(item)
-
-        resp = self.app.get('/pictures/{}'.format(uuid.uuid4()))
-        assert resp.status_code == NOT_FOUND
 
     def test_delete_picture_succed(self):
         picture1 = self.create_item_picture()
@@ -46,4 +69,5 @@ class TestPicture(BaseTest):
         assert resp.status_code == NOT_FOUND
 
     def test_delete_picture_failure_not_found(self):
-        pass
+        resp = self.app.get('/picture/{}'.format(uuid.uuid4()))
+        assert resp.status_code == NOT_FOUND
