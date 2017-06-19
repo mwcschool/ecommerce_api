fc0610fe8aeaca64790f713806f9cb2155e164d8
Luca Bandini <luca.bandini@outlook.it>
2017-06-19 11:40:29 +0200

    comparison hash code between image from get and image on directory

--- tests/test_picture.py
+++ tests/test_picture.py
@@ -1,14 +1,9 @@
-import json
-from peewee import SqliteDatabase
-from http.client import CREATED
 from http.client import NO_CONTENT
 from http.client import NOT_FOUND
 from http.client import OK
-from http.client import BAD_REQUEST
 from .base_test import BaseTest
 from models import Picture
-from models import Item
-from app import app
+import hashlib
 import uuid
 import os
 
@@ -19,45 +14,24 @@ class TestPicture(BaseTest):
         picture1 = self.create_item_picture()
 
         resp = self.app.get('/pictures/{}'.format(picture1.uuid))
+
+        m = hashlib.sha256()
+        m.update(resp.data)
+        hash_response = m.digest()
+
+        with open(os.path.join('.', 'tests', 'images', 'test_image.png'), 'rb') as image:
+            image = image.read()
+
+            h4sh = hashlib.sha256()
+            h4sh.update(image)
+            hash_image = h4sh.digest()
+
         assert resp.status_code == OK
-        #assert json.loads(resp.data.decode()) == [picture1.json()]
+        assert hash_response == hash_image
 
     def test_get_picture_not_existing_picture(self):
         resp = self.app.get('/pictures/{}'.format(uuid.uuid4()))
         assert resp.status_code == NOT_FOUND
-
-    def test_get_picture_wrong_item(self):
-        item1 = self.create_item()
-        item2 = self.create_item()
-        picture1 = self.create_item_picture(item1)
-        #import pdb; pdb.set_trace()
-
-        resp = self.app.get('/pictures/{}'.format(item2.uuid))
-        assert resp.status_code == NOT_FOUND
-
-    def test_get_picture_int_uuid(self):
-        picture1 = self.create_item_picture()
-
-        resp = self.app.get('/pictures/{}'.format(int(picture1.uuid)))
-        assert resp.status_code == NOT_FOUND
-
-    def test_get_picture_wrong_extension(self):
-        test_image_path= os.path.join('.', 'tests', 'images', 'test_image.png')
-
-        picture1 = self.create_item_picture(extension='jpeg')
-
-        resp = self.app.get('/pictures/{}'.format(picture1.uuid))
-        assert resp.status_code == NOT_FOUND
-
-
-    #def test_image_path():
-        #pass
-
-    # def test_get_picture_wrong_extension(self):
-    #     picture1 = self.create_item_picture(extension='png')
-
-    #     resp = self.app.get('/pictures/{}'.format(picture1.uuid))
-    #     assert resp.status_code == BAD_REQUEST
 
     def test_delete_picture_succed(self):
         picture1 = self.create_item_picture()
@@ -69,5 +43,5 @@ class TestPicture(BaseTest):
         assert resp.status_code == NOT_FOUND
 
     def test_delete_picture_failure_not_found(self):
-        resp = self.app.get('/picture/{}'.format(uuid.uuid4()))
+        resp = self.app.delete('/picture/{}'.format(uuid.uuid4()))
         assert resp.status_code == NOT_FOUND
