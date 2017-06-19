bdb08ce1e76c49cc9587b7dc6b83624df7a79836
Vincenzo Volpe <vincenzovolpe10@icloud.com>
2017-06-19 12:12:46 +0200

    Changed const=os.urandom(seed) in default=os.urandom(10).

--- scripts/demo-content.py
+++ scripts/demo-content.py
@@ -7,13 +7,13 @@ import os
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('seed', type=int, const=os.urandom(seed))
+    parser.add_argument('seed', type=int, default=os.urandom(10))
     args = parser.parse_args()
     database.connect()
+
     seed(args.seed)
     fake = Factory.create('it_IT')
     fake.seed(args.seed)
-    os.urandom(seed)
 
     items_list = []
     users_list = []
