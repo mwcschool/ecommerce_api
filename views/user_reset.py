0d5147eed2651170b069429d3bba86b748437d00
Francesco Cambi <francesco100@hotmail.it>
2017-06-20 11:38:47 +0200

    Flake8 <3

--- views/user_reset.py
+++ views/user_reset.py
@@ -1,7 +1,8 @@
 from models import Reset
 from flask_restful import Resource, reqparse
 import utils
-from http.client import NOT_FOUND
+from http.client import NOT_FOUND, OK
+from datetime import datetime
 
 
 class PasswordResetResource(Resource):
@@ -13,7 +14,7 @@ class PasswordResetResource(Resource):
 
         try:
             reset_code = Reset.get(Reset.uuid == args['code']
-                                   and Reset.enable == True
+                                   and Reset.enable
                                    and Reset.expiration_date > datetime.now()
                                    )
         except (Reset.DoesNotExists):
