ad457c606cb4828ae949a81cedb38a5b4bee6e9c
Korda Iryna <irynakorda@gmail.com>
2017-06-21 15:04:18 +0200

    Changed reqparce with function verify_json

--- views/address.py
+++ views/address.py
@@ -2,34 +2,30 @@ import uuid
 from models import Address
 from http.client import CREATED, NOT_FOUND, NO_CONTENT, BAD_REQUEST, OK
 from flask_restful import Resource, reqparse
-from flask import g
+from flask import g, request
 import utils
 import auth
+
+from jsonschema import ValidationError
 
 
 class AddressesResource(Resource):
     @auth.login_required
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('nation', type=utils.non_empty_str, required=True)
-        parser.add_argument('city', type=utils.non_empty_str, required=True)
-        parser.add_argument('postal_code', type=utils.non_empty_str, required=True)
-        parser.add_argument('local_address', type=utils.non_empty_str, required=True)
-        parser.add_argument('phone', type=utils.non_empty_str, required=True)
-        args = parser.parse_args(strict=True)
-
-        for parm in ['nation', 'city', 'postal_code', 'local_address', 'phone']:
-            if len(args[parm]) < 3:
-                return '', BAD_REQUEST
+        json = request.get_json()
+        try:
+            Address.verify_json(json)
+        except ValidationError as err:
+            return {"message": err.message}, NOT_FOUND
 
         address = Address.create(
             uuid=uuid.uuid4(),
             user=g.current_user,
-            nation=args['nation'],
-            city=args['city'],
-            postal_code=args['postal_code'],
-            local_address=args['local_address'],
-            phone=args['phone'],
+            nation=json['nation'],
+            city=json['city'],
+            postal_code=json['postal_code'],
+            local_address=json['local_address'],
+            phone=json['phone'],
         )
 
         return address.json(), CREATED
