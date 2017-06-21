a4819782e2e326af6815461d330b381084b1eb30
Filippo Vecchi <fv284@hotmail.com>
2017-06-21 12:42:41 +0200

    Fixed reset query in user_reset.py

--- views/user_reset.py
+++ views/user_reset.py
@@ -20,19 +20,20 @@ class PasswordResetResource(Resource):
         args = parser.parse_args(strict=True)
 
         try:
-            reset_code = Reset.get(
-                Reset.uuid == args['code']
-                and Reset.enable
-                and Reset.expiration_date > datetime.now()
-                and Reset.user.superuser == False
-            )
-        except (Reset.DoesNotExists):
+            user = Reset.get(Reset.uuid == args['code']).user
+            reset_code = Reset.select()\
+                .where(Reset.uuid == args['code'])\
+                .where(Reset.enable)\
+                .where(Reset.expiration_date > datetime.now()).get()
+
+        except Reset.DoesNotExist:
+            return None, NOT_FOUND
+
+        if user.superuser:
             return None, NOT_FOUND
 
         if not len(args['password']) > 6:
             return None, BAD_REQUEST
-
-        user = reset_code.user
 
         reset_code.enable = False
         user.password = crypt_password(args['password'])
