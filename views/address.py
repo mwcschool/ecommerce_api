f818c73c17dad92927ee5626223107458d62c4e0
Korda Iryna <irynakorda@gmail.com>
2017-06-23 09:34:38 +0200

    Modified function put, deleted reqparse

--- views/address.py
+++ views/address.py
@@ -1,12 +1,12 @@
 import uuid
 from models import Address
 from http.client import CREATED, NOT_FOUND, NO_CONTENT, BAD_REQUEST, OK
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from flask import g, request
-import utils
 import auth
-
 from jsonschema import ValidationError
+
+# import utils
 
 
 class AddressesResource(Resource):
@@ -47,6 +47,7 @@ class AddressResource(Resource):
 
     @auth.login_required
     def put(self, address_id):
+        json = request.get_json()
         try:
             address = (
                 Address.select()
@@ -56,24 +57,17 @@ class AddressResource(Resource):
         except Address.DoesNotExist:
             return None, NOT_FOUND
 
-        parser = reqparse.RequestParser()
-        parser.add_argument('nation', type=utils.non_empty_str, required=True)
-        parser.add_argument('city', type=utils.non_empty_str, required=True)
-        parser.add_argument('postal_code', type=utils.non_empty_str, required=True)
-        parser.add_argument('local_address', type=utils.non_empty_str, required=True)
-        parser.add_argument('phone', type=utils.non_empty_str, required=True)
-        args = parser.parse_args(strict=True)
-
-        for parm in ['nation', 'city', 'postal_code', 'local_address', 'phone']:
-            if len(args[parm]) < 3:
-                return '', BAD_REQUEST
+        try:
+            Address.verify_json(json)
+        except ValidationError as err:
+            return {"message": err.message}, BAD_REQUEST
 
         if address.user.uuid == g.current_user.uuid:
-            address.nation = args['nation']
-            address.city = args['city']
-            address.postal_code = args['postal_code']
-            address.local_address = args['local_address']
-            address.phone = args['phone']
+            address.nation = json['nation']
+            address.city = json['city']
+            address.postal_code = json['postal_code']
+            address.local_address = json['local_address']
+            address.phone = json['phone']
             address.save()
 
             return address.json(), CREATED
