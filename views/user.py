95b4840c32f167b384528bf2410b23bf2322ed72
Vincenzo Volpe <vincenzovolpe10@icloud.com>
2017-07-21 11:51:21 +0200

    Updated class UsersResource(Resource).

--- views/user.py
+++ views/user.py
@@ -1,12 +1,13 @@
 import uuid
 from models import User
 import auth
-from flask import g
+from flask import g, request
 from http.client import CREATED, NOT_FOUND, NO_CONTENT, BAD_REQUEST, UNAUTHORIZED
 from flask_restful import Resource, reqparse
 import re
 from passlib.hash import pbkdf2_sha256
 import utils
+from marshmallow import ValidationError
 
 
 def valid_email(email):
@@ -21,20 +22,19 @@ def crypt_password(password):
 
 class UsersResource(Resource):
     def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('first_name', type=utils.non_empty_str, required=True)
-        parser.add_argument('last_name', type=utils.non_empty_str, required=True)
-        parser.add_argument('email', type=utils.non_empty_str, required=True)
-        parser.add_argument('password', type=utils.non_empty_str, required=True)
-        args = parser.parse_args(strict=True)
+        json_data = request.get_json()
+        try:
+            User.verify_json(json_data)
+        except ValidationError as err:
+            return {'message': err.message}, NOT_FOUND
 
-        if valid_email(args['email']) and len(args['password']) > 6:
+        if valid_email(json_data['email']) and len(json_data['password']) > 6:
             obj = User.create(
                 uuid=uuid.uuid4(),
-                first_name=args['first_name'],
-                last_name=args['last_name'],
-                email=args['email'],
-                password=crypt_password(args['password'])
+                first_name=json_data['first_name'],
+                last_name=json_data['last_name'],
+                email=json_data['email'],
+                password=crypt_password(json_data['password'])
             )
 
             return obj.json(), CREATED
@@ -53,12 +53,12 @@ class UserResource(Resource):
         if obj != g.current_user:
             return '', UNAUTHORIZED
 
-        parser = reqparse.RequestParser()
-        parser.add_argument('first_name', type=utils.non_empty_str, required=True)
-        parser.add_argument('last_name', type=utils.non_empty_str, required=True)
-        parser.add_argument('email', type=utils.non_empty_str, required=True)
-        parser.add_argument('password', type=utils.non_empty_str, required=True)
-        args = parser.parse_args(strict=True)
+        #parser = reqparse.RequestParser()
+        #parser.add_argument('first_name', type=utils.non_empty_str, required=True)
+        #parser.add_argument('last_name', type=utils.non_empty_str, required=True)
+        #parser.add_argument('email', type=utils.non_empty_str, required=True)
+        #parser.add_argument('password', type=utils.non_empty_str, required=True)
+        #args = parser.parse_args(strict=True)
 
         if valid_email(args['email']) is not None and len(args['password']) > 6:
             obj.first_name = args['first_name']
