c64122f8b49b612930349906ab25aa5503ee4d51
F.Vecchi <fv284@hotmail.com>
2017-07-25 16:01:28 +0200

    Inserted proper schema in method, added 'if' block to bypass 'marshmallowschema<->jsonschema' conversion bug

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from peewee import Model, SqliteDatabase, PostgresqlDatabase, Check
 from peewee import DecimalField, TextField, CharField
 from peewee import UUIDField, ForeignKeyField, IntegerField, BooleanField
 from schemas import ItemSchema, UserSchema, AddressSchema
-from schemas import OrderSchema, OrderItemSchema, FavoritesSchema
+from schemas import OrderSchema, OrderItemSchema, FavoritesSchema, ItemPatchSchema
 from passlib.hash import pbkdf2_sha256
 from jsonschema import validate
 from marshmallow_jsonschema import JSONSchema
@@ -47,6 +47,8 @@ class BaseModel(Model):
     def verify_json(cls, json, partial=False):
         schema = cls.get_partial_schema() if partial else cls.get_schema()
         json_schema = JSONSchema().dump(schema).data
+        if partial:
+            del json_schema['required']
         validate(json, json_schema)
 
     def json(self):
@@ -72,7 +74,8 @@ class Item(BaseModel):
 
     @classmethod
     def get_partial_schema(cls):
-        return PartialItemSchema()
+        return ItemPatchSchema()
+
 
 class User(BaseModel):
     uuid = UUIDField(unique=True)
